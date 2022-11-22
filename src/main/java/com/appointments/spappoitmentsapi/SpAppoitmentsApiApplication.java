136e49c32845d6b9210c7ac9df97d4f9d0f6d805
Gustavo <penarandagustavo5@gmail.com>
2022-11-22 15:29:10 -0500

    Project created

---
+++ src/main/java/com/appointments/spappoitmentsapi/SpAppoitmentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.appointments.spappoitmentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAppoitmentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAppoitmentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appointments/spappoitmentsapi/SpAppoitmentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointments.spappoitmentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpAppoitmentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
