e86a65b3924626bf406bb7f2bc47f4225cd1287d
Gustavo <penarandagustavo5@gmail.com>
2022-11-25 15:48:05 -0500

    Add a Test Controller

---
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/TestController.java
@@ -0,0 +1,9 @@
+package com.appointments.spappoitmentsapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/controller/tests")
+public class TestController {
+}
