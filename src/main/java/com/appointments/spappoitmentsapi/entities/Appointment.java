8f9aa3068b0ba260ebfbf020538dabbe63f7180e
Gustavo <penarandagustavo5@gmail.com>
2022-11-25 13:31:35 -0500

    change getters setters and constructors for lombok annotations

--- src/main/java/com/appointments/spappoitmentsapi/entities/Appointment.java
+++ src/main/java/com/appointments/spappoitmentsapi/entities/Appointment.java
@@ -2,108 +2,43 @@ package com.appointments.spappoitmentsapi.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Appointments")
 public class Appointment {
     //attributes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
     @JsonProperty("dateAppointment")
+    @Getter @Setter
     private LocalDate dateAppointment;
 
     @JsonFormat(pattern = "HH:mm")
     @JsonProperty("hourAppointment")
+    @Getter @Setter
     private LocalTime hourAppointment;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @JoinColumn(name = "id_affiliate")
+    @Getter @Setter
     private Affiliate affiliate;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @JoinColumn(name = "id_test")
+    @Getter @Setter
     private Test test;
-
-    //constructors
-
-    public Appointment() {
-    }
-
-    //getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getDate() {
-        return dateAppointment;
-    }
-
-    public void setDate(LocalDate date) {
-        this.dateAppointment = date;
-    }
-
-    public LocalTime getHour() {
-        return hourAppointment;
-    }
-
-    public void setHour(LocalTime hour) {
-        this.hourAppointment = hour;
-    }
-
-    public LocalDate getDateAppointment() {
-        return dateAppointment;
-    }
-
-    public void setDateAppointment(LocalDate dateAppointment) {
-        this.dateAppointment = dateAppointment;
-    }
-
-    public LocalTime getHourAppointment() {
-        return hourAppointment;
-    }
-
-    public void setHourAppointment(LocalTime hourAppointment) {
-        this.hourAppointment = hourAppointment;
-    }
-
-    public Affiliate getAffiliate() {
-        return affiliate;
-    }
-
-    public void setAffiliate(Affiliate affiliate) {
-        this.affiliate = affiliate;
-    }
-
-    public Test getTest() {
-        return test;
-    }
-
-    public void setTest(Test test) {
-        this.test = test;
-    }
-
-    //toString
-
-    @Override
-    public String toString() {
-        return "Appointments{" +
-                "id=" + id +
-                ", date=" + dateAppointment +
-                ", hour=" + hourAppointment +
-                '}';
-    }
 }
