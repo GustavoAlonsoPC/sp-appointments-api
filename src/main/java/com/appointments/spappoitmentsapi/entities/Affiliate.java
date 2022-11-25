d25cc5c0488de49bffc059cb75794c2b6d69e781
Gustavo <penarandagustavo5@gmail.com>
2022-11-25 15:17:19 -0500

    change to lombok getters, setters and constructors

--- src/main/java/com/appointments/spappoitmentsapi/entities/Affiliate.java
+++ src/main/java/com/appointments/spappoitmentsapi/entities/Affiliate.java
@@ -2,90 +2,31 @@ package com.appointments.spappoitmentsapi.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jdk.dynalink.linker.LinkerServices;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Affiliate {
 
     //attributes
     @Id
+    @Getter @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private Integer age;
+    @Getter @Setter
     private String mail;
 
     @OneToMany(orphanRemoval = true)
     @JoinColumn(name = "id_affiliate")
     @JsonIgnore
     private List<Appointment> appointments;
-
-    //constructors
-
-    public Affiliate() {
-    }
-
-    public Affiliate(Long id, String name, Integer age, String mail) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.mail = mail;
-    }
-
-    //getter and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public List<Appointment> getAppointments() {
-        return appointments;
-    }
-
-    public void setAppointments(List<Appointment> appointments) {
-        this.appointments = appointments;
-        for (Appointment appointment : appointments) appointment.setAffiliate(this);
-    }
-
-    //toString
-
-    @Override
-    public String toString() {
-        return "Affiliates{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", mail='" + mail + '\'' +
-                '}';
-    }
 }
