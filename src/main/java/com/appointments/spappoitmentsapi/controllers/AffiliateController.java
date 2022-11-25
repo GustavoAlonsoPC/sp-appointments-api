e4c311a761f992a63b684436d85ea3ec1187dbd9
Gustavo <penarandagustavo5@gmail.com>
2022-11-25 15:44:59 -0500

    add an affiliate controller

---
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/AffiliateController.java
@@ -0,0 +1,11 @@
+package com.appointments.spappoitmentsapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/controller/affiliates")
+public class AffiliateController {
+    
+}
