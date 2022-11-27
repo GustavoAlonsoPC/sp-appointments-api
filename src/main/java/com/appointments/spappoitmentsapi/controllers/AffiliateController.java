2728ae08d0393e2d17133d9679e88afa58e8c746
Gustavo <penarandagustavo5@gmail.com>
2022-11-27 17:25:27 -0500

    return a Response Entity instead a plain Affiliates List

--- src/main/java/com/appointments/spappoitmentsapi/controllers/AffiliateController.java
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/AffiliateController.java
@@ -4,6 +4,8 @@ import com.appointments.spappoitmentsapi.dto.AffiliateDTO;
 import com.appointments.spappoitmentsapi.entities.Affiliate;
 import com.appointments.spappoitmentsapi.services.AffiliateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,9 @@ public class AffiliateController {
     }
 
     @GetMapping
-    public List<AffiliateDTO> getAllAffiliates() {
-        return affiliateService.getAll();
+    public ResponseEntity<List<AffiliateDTO>> getList() {
+        List<AffiliateDTO> affiliateDTOList = affiliateService.getAll();
+        if (affiliateDTOList.isEmpty()) return ResponseEntity.noContent().build();
+        return new ResponseEntity<List<AffiliateDTO>>(affiliateDTOList, HttpStatus.OK);
     }
 }
