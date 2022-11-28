7004d39d25399f9b19a6ed677b81b33c49374aa0
Gustavo <penarandagustavo5@gmail.com>
2022-11-28 10:40:59 -0500

    add model mapper into TestService

--- src/main/java/com/appointments/spappoitmentsapi/services/TestService.java
+++ src/main/java/com/appointments/spappoitmentsapi/services/TestService.java
@@ -2,6 +2,7 @@ package com.appointments.spappoitmentsapi.services;
 
 import com.appointments.spappoitmentsapi.entities.Test;
 import com.appointments.spappoitmentsapi.repositories.TestRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,13 @@ import java.util.List;
 @Service
 public class TestService {
     private final TestRepository testRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public TestService(TestRepository testRepository) {
+    public TestService(TestRepository testRepository, ModelMapper modelMapper) {
+
         this.testRepository = testRepository;
+        this.modelMapper = modelMapper;
     }
 
 }
