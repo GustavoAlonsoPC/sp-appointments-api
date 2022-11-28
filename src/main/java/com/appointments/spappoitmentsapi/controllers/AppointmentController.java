abef0702118344cd8de48e516b39d93773dee8e3
Gustavo <penarandagustavo5@gmail.com>
2022-11-28 12:50:02 -0500

    add Service Layer into AppointmentController

--- src/main/java/com/appointments/spappoitmentsapi/controllers/AppointmentController.java
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/AppointmentController.java
@@ -1,10 +1,20 @@
 package com.appointments.spappoitmentsapi.controllers;
 
+import com.appointments.spappoitmentsapi.services.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/controller/appointments")
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
     
 }
