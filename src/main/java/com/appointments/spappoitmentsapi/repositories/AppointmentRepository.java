537277f4a66e07bb0a93b4663c2734b584baaa8c
Gustavo <penarandagustavo5@gmail.com>
2022-11-28 16:59:40 -0500

    Declare getAppointmentByAffiliateId in Appointment Repository

--- src/main/java/com/appointments/spappoitmentsapi/repositories/AppointmentRepository.java
+++ src/main/java/com/appointments/spappoitmentsapi/repositories/AppointmentRepository.java
@@ -2,6 +2,12 @@ package com.appointments.spappoitmentsapi.repositories;
 
 import com.appointments.spappoitmentsapi.entities.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @Query("SELECT A FROM Appointment A WHERE A.affiliate.id = ?1")
+    List<Appointment> getAppointmentByAffiliateId (Long IdAffiliate);
 }
