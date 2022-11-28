bfbba163c70b8835d19c2e9945e7fc3b23720b7b
Gustavo <penarandagustavo5@gmail.com>
2022-11-28 13:49:07 -0500

    getList method in appointment service

--- src/main/java/com/appointments/spappoitmentsapi/services/AppointmentService.java
+++ src/main/java/com/appointments/spappoitmentsapi/services/AppointmentService.java
@@ -1,9 +1,14 @@
 package com.appointments.spappoitmentsapi.services;
 
+import com.appointments.spappoitmentsapi.dto.AppointmentsDTO;
+import com.appointments.spappoitmentsapi.entities.Appointment;
 import com.appointments.spappoitmentsapi.repositories.AppointmentRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AppointmentService {
@@ -15,5 +20,15 @@ public class AppointmentService {
     public AppointmentService(AppointmentRepository appointmentRepository, ModelMapper modelMapper) {
         this.appointmentRepository = appointmentRepository;
         this.modelMapper = modelMapper;
+    }
+
+    public List<AppointmentsDTO> getAll() {
+        List<Appointment> appointments = appointmentRepository.findAll();
+        List<AppointmentsDTO> appointmentsDTOList = new ArrayList<>();
+        for (Appointment app : appointments) {
+            AppointmentsDTO appointmentsDTO = modelMapper.map(app, AppointmentsDTO.class);
+            appointmentsDTOList.add(appointmentsDTO);
+        }
+        return appointmentsDTOList;
     }
 }
