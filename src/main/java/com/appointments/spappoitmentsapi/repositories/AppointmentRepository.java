3e73498b142c39a49e71b009a3f6b01b621a9d23
Gustavo <penarandagustavo5@gmail.com>
2022-11-28 19:20:23 -0500

    return list of appointments by date

--- src/main/java/com/appointments/spappoitmentsapi/repositories/AppointmentRepository.java
+++ src/main/java/com/appointments/spappoitmentsapi/repositories/AppointmentRepository.java
@@ -4,10 +4,14 @@ import com.appointments.spappoitmentsapi.entities.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
     @Query("SELECT A FROM Appointment A WHERE A.affiliate.id = ?1")
     List<Appointment> getAppointmentByAffiliateId (Long IdAffiliate);
+
+    @Query("SELECT A FROM Appointment A WHERE A.dateAppointment = ?1 ORDER BY A.affiliate.id")
+    List<Appointment> getAppointmentByDateAppointment(LocalDate localDate);
 }
