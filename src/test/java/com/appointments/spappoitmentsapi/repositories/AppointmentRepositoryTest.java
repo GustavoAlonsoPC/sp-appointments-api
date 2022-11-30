d8a470e87360fc082e515bdd5e511dca1ed46937
Gustavo <penarandagustavo5@gmail.com>
2022-11-30 10:03:36 -0500

    add AppointmentRepositoryTest

---
+++ src/test/java/com/appointments/spappoitmentsapi/repositories/AppointmentRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.appointments.spappoitmentsapi.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentRepositoryTest {
+
+    @Test
+    void getAppointmentByAffiliateId() {
+    }
+
+    @Test
+    void getAppointmentByDate() {
+    }
+}
