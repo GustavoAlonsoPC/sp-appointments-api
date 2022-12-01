399649d347219b39b5b350f8189f02f9377e42cd
Gustavo <penarandagustavo5@gmail.com>
2022-12-01 09:14:04 -0500

    add AffiliateServiceTest

---
+++ src/test/java/com/appointments/spappoitmentsapi/services/AffiliateServiceTest.java
@@ -0,0 +1,28 @@
+package com.appointments.spappoitmentsapi.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AffiliateServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void getByID() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
