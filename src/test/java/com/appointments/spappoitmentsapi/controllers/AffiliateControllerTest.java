58f195b8a9942ac4ffeb902a73b4205fe05ccfc5
Gustavo <penarandagustavo5@gmail.com>
2022-12-01 13:59:44 -0500

    add all controllers tests files

---
+++ src/test/java/com/appointments/spappoitmentsapi/controllers/AffiliateControllerTest.java
@@ -0,0 +1,28 @@
+package com.appointments.spappoitmentsapi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AffiliateControllerTest {
+
+    @Test
+    void getList() {
+    }
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void getByID() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/com/appointments/spappoitmentsapi/controllers/AppointmentControllerTest.java
@@ -0,0 +1,36 @@
+package com.appointments.spappoitmentsapi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentControllerTest {
+
+    @Test
+    void getList() {
+    }
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void getByID() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getByAffiliateID() {
+    }
+
+    @Test
+    void getByDate() {
+    }
+}
