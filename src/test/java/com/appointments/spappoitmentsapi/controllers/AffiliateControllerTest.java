5f77140e2b89eaf2275763c4aabc08277bbf756d
Gustavo <penarandagustavo5@gmail.com>
2022-12-02 09:03:14 -0500

    AffiliateController getList method tested when non-empty list

--- src/test/java/com/appointments/spappoitmentsapi/controllers/AffiliateControllerTest.java
+++ src/test/java/com/appointments/spappoitmentsapi/controllers/AffiliateControllerTest.java
@@ -1,13 +1,63 @@
 package com.appointments.spappoitmentsapi.controllers;
 
+import com.appointments.spappoitmentsapi.dto.AffiliateDTO;
+import com.appointments.spappoitmentsapi.services.AffiliateService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AffiliateControllerTest {
 
+    @Mock
+    private AffiliateService affiliateServiceMock;
+
+    @InjectMocks
+    AffiliateController underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new AffiliateController(affiliateServiceMock);
+    }
+
     @Test
-    void getList() {
+    void getNotEmptyList() {
+        List<AffiliateDTO> affiliateDTOListMocked = new ArrayList<>();
+        AffiliateDTO aff1 = new AffiliateDTO();
+        AffiliateDTO aff2 = new AffiliateDTO();
+
+        aff1.setId(1L);
+        aff1.setName("AffName1");
+        aff1.setMail("affMail1");
+        aff1.setAge(23);
+
+        aff2.setId(2L);
+        aff2.setName("AffName2");
+        aff2.setMail("affMail2");
+        aff2.setAge(24);
+
+        affiliateDTOListMocked.add(aff1);
+        affiliateDTOListMocked.add(aff2);
+
+        when(affiliateServiceMock.getAll()).thenReturn(affiliateDTOListMocked);
+        ResponseEntity<List<AffiliateDTO>> response = underTest.getList();
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(affiliateDTOListMocked);
+
     }
 
     @Test
