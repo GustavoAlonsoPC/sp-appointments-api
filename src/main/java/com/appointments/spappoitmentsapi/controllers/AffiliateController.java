ab3c4e030d607cd47966f5ded7959c3760c6ae7d
Gustavo <penarandagustavo5@gmail.com>
2022-12-05 15:46:01 -0500

    add extra documentation to AffiliateController

--- src/main/java/com/appointments/spappoitmentsapi/controllers/AffiliateController.java
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/AffiliateController.java
@@ -2,11 +2,16 @@ package com.appointments.spappoitmentsapi.controllers;
 
 import com.appointments.spappoitmentsapi.dto.AffiliateDTO;
 import com.appointments.spappoitmentsapi.services.AffiliateService;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,6 +29,9 @@ public class AffiliateController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Get all affiliates", notes = "Retrieves all the affiliates stored in the database")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Affiliates retrieved"),
+                            @ApiResponse(code = 204, message = "No affiliates found")})
     public ResponseEntity<List<AffiliateDTO>> getList() {
         List<AffiliateDTO> affiliateDTOList = affiliateService.getAll();
         if (affiliateDTOList.isEmpty()) return ResponseEntity.noContent().build();
@@ -31,6 +39,9 @@ public class AffiliateController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a new affiliate", notes = "Creates a new affiliate")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Successfully created"),
+                            @ApiResponse(code = 400, message = "Invalid input")})
     public ResponseEntity<AffiliateDTO> post(@RequestBody AffiliateDTO affiliateDTO) {
         if (affiliateDTO == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         affiliateDTO = affiliateService.post(affiliateDTO);
@@ -38,12 +49,18 @@ public class AffiliateController {
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Return an affiliate by ID", notes = "Need an ID paramenter in path")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Affiliate found"),
+            @ApiResponse(code = 404, message = "Affiliates does not exists")})
     public ResponseEntity<AffiliateDTO> getByID(@PathVariable Long id) {
         AffiliateDTO affiliateDTO = affiliateService.getByID(id);
         return affiliateDTO == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<AffiliateDTO>(affiliateDTO, HttpStatus.OK);
     }
 
     @PutMapping
+    @ApiOperation(value = "Updates an existing affiliate", notes = "Needs an ID parameter in query")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Successfully updated"),
+            @ApiResponse(code = 400, message = "Invalid input")})
     public ResponseEntity<AffiliateDTO> put(@RequestBody AffiliateDTO affiliateDTO) {
         if (affiliateDTO == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         affiliateDTO = affiliateService.put(affiliateDTO);
@@ -51,6 +68,10 @@ public class AffiliateController {
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deletes an affiliate", notes = "Deletes an affiliate, if the affiliate is referenced in another entity the deletion can't be completed")
+    @ApiResponses(value = {@ApiResponse(code = 204, message = "Successfully deleted"),
+            @ApiResponse(code = 404, message = "Affiliate does not exists"),
+            @ApiResponse(code = 500, message = "Affiliate is referenced in other entities (children)")})
     public ResponseEntity delete(@PathVariable Long id) {
         try {
             return affiliateService.delete(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
--- src/main/java/com/appointments/spappoitmentsapi/dto/AffiliateDTO.java
+++ src/main/java/com/appointments/spappoitmentsapi/dto/AffiliateDTO.java
@@ -1,12 +1,15 @@
 package com.appointments.spappoitmentsapi.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel
 public class AffiliateDTO {
 
-    private Long id;
-    private String name;
-    private Integer age;
-    private String mail;
+    @ApiModelProperty(readOnly = true) private Long id;
+    @ApiModelProperty private String name;
+    @ApiModelProperty private Integer age;
+    @ApiModelProperty private String mail;
 }
