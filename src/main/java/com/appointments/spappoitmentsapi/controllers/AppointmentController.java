ab3918d5b6efda16939496678d18c0d6e8387b77
Gustavo <penarandagustavo5@gmail.com>
2022-12-09 17:09:28 -0500

    swagger docs appointments controller

--- src/main/java/com/appointments/spappoitmentsapi/controllers/AppointmentController.java
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/AppointmentController.java
@@ -2,6 +2,9 @@ package com.appointments.spappoitmentsapi.controllers;
 
 import com.appointments.spappoitmentsapi.dto.AppointmentDTO;
 import com.appointments.spappoitmentsapi.services.AppointmentService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,9 @@ public class AppointmentController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Get all appointments", notes = "Retrieves all the appointments stored from the database")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Appointments retrieved"),
+            @ApiResponse(code = 204, message = "No appointments found")})
     public ResponseEntity<List<AppointmentDTO>> getList() {
         List<AppointmentDTO> appointmentDTOList = appointmentService.getAll();
         if (appointmentDTOList.isEmpty()) return ResponseEntity.noContent().build();
@@ -29,29 +35,45 @@ public class AppointmentController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a new appointment", notes = "Creates a new appointment")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Successfully created"),
+            @ApiResponse(code = 400, message = "Invalid input")})
     public ResponseEntity<AppointmentDTO> post(@RequestBody AppointmentDTO appointmentDTO) {
         appointmentDTO = appointmentService.post(appointmentDTO);
         return appointmentDTO == null ? new ResponseEntity<>(HttpStatus.BAD_REQUEST) : new ResponseEntity<>(appointmentDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Return an appointment by ID", notes = "Need an ID parameter in path")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "appointment found"),
+            @ApiResponse(code = 404, message = "appointment does not exists")})
     public ResponseEntity<AppointmentDTO> getByID(@PathVariable Long id) {
         AppointmentDTO appointmentDTO = appointmentService.getByID(id);
         return appointmentDTO == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(appointmentDTO, HttpStatus.OK);
     }
 
     @PutMapping
+    @ApiOperation(value = "Updates an existing appointment", notes = "Needs an ID parameter in query")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Successfully updated"),
+            @ApiResponse(code = 400, message = "Invalid input")})
     public ResponseEntity<AppointmentDTO> put(@RequestBody AppointmentDTO appointmentDTO) {
         appointmentDTO = appointmentService.put(appointmentDTO);
         return appointmentDTO == null ? new ResponseEntity<>(HttpStatus.BAD_REQUEST) : new ResponseEntity<>(appointmentDTO, HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deletes an appointment", notes = "Deletes an appointment")
+    @ApiResponses(value = {@ApiResponse(code = 204, message = "Successfully deleted"),
+            @ApiResponse(code = 404, message = "Appointment does not exists")})
     public ResponseEntity delete(@PathVariable Long id) {
         return appointmentService.delete(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
     @GetMapping(path = "/aff",params = "idAffiliate")
+    @ApiOperation(value = "Gets by affiliate", notes = "Gets all appointments of a given affiliate")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully consulted"),
+            @ApiResponse(code = 404, message = "Appointment does not exists"),
+            @ApiResponse(code = 204, message = "aff does not have appointments")})
     public ResponseEntity<List<AppointmentDTO>> getByAffiliateID(@RequestParam Long idAffiliate) {
         List<AppointmentDTO> appointmentDTOList = appointmentService.getByAffiliateID(idAffiliate);
         if (appointmentDTOList == null) return ResponseEntity.notFound().build();
@@ -60,6 +82,9 @@ public class AppointmentController {
     }
 
     @GetMapping(path = "/date", params = "dateAppointment")
+    @ApiOperation(value = "Gets by date", notes = "Gets all appointments of a given date")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully consulted"),
+            @ApiResponse(code = 204, message = "date does not have appointments")})
     public ResponseEntity<List<AppointmentDTO>> getByDate(@RequestParam LocalDate dateAppointment) {
         List<AppointmentDTO> appointmentDTOList = appointmentService.getByDate(dateAppointment);
         if (appointmentDTOList.isEmpty()) return ResponseEntity.noContent().build();
@@ -67,6 +92,10 @@ public class AppointmentController {
     }
 
     @DeleteMapping
+    @ApiOperation(value = "Deletes by affiliate or test", notes = "Deletes all appointments of a given affiliate or test")
+    @ApiResponses(value = {@ApiResponse(code = 204, message = "Successfully deleted"),
+            @ApiResponse(code = 400, message = "Bad request")})
+
     public ResponseEntity deleteByAffiliateOrTest(@RequestParam(required = false) Long affiliateId,
                                             @RequestParam(required = false) Long testId) {
         if (affiliateId != null && testId != null) return ResponseEntity.badRequest().build();
--- src/main/java/com/appointments/spappoitmentsapi/dto/AppointmentDTO.java
+++ src/main/java/com/appointments/spappoitmentsapi/dto/AppointmentDTO.java
@@ -1,19 +1,22 @@
 package com.appointments.spappoitmentsapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
+@ApiModel
 public class AppointmentDTO {
 
-    private Long id;
+    @ApiModelProperty(readOnly = true) private Long id;
     @JsonFormat(pattern = "dd/MM/yyyy")
-    private LocalDate dateAppointment;
+    @ApiModelProperty(dataType = "string", example = "01/01/2023") private LocalDate dateAppointment;
     @JsonFormat(pattern = "HH:mm")
-    private LocalTime hourAppointment;
+    @ApiModelProperty(dataType = "string", example = "12:00")private LocalTime hourAppointment;
     private Long idTest;
     private Long idAffiliate;
 }
