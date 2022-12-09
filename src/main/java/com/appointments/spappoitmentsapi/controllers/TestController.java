686bd47a09f6a5f0bd466b98707ff072b5d3ba62
Gustavo <penarandagustavo5@gmail.com>
2022-12-09 16:12:25 -0500

    swagger doc updated

--- src/main/java/com/appointments/spappoitmentsapi/controllers/TestController.java
+++ src/main/java/com/appointments/spappoitmentsapi/controllers/TestController.java
@@ -2,6 +2,9 @@ package com.appointments.spappoitmentsapi.controllers;
 
 import com.appointments.spappoitmentsapi.dto.TestDTO;
 import com.appointments.spappoitmentsapi.services.TestService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -24,6 +27,9 @@ public class TestController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Get all tests", notes = "Retrieves all the tests stored from the database")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Tests retrieved"),
+            @ApiResponse(code = 204, message = "No tests found")})
     public ResponseEntity<List<TestDTO>> getList() {
         List<TestDTO> testDTOList = testService.getAll();
         if (testDTOList.isEmpty()) return ResponseEntity.noContent().build();
@@ -31,6 +37,9 @@ public class TestController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a new test", notes = "Creates a new test")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Successfully created"),
+            @ApiResponse(code = 400, message = "Invalid input")})
     public ResponseEntity<TestDTO> post(@RequestBody TestDTO testDTO) {
         if (testDTO == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         testDTO = testService.post(testDTO);
@@ -38,12 +47,18 @@ public class TestController {
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Return a test by ID", notes = "Need an ID paramenter in path")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "test found"),
+            @ApiResponse(code = 404, message = "test does not exists")})
     public ResponseEntity<TestDTO> getByID(@PathVariable Long id) {
         TestDTO testDTO = testService.getByID(id);
         return testDTO == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(testDTO, HttpStatus.OK);
     }
 
     @PutMapping
+    @ApiOperation(value = "Updates an existing test", notes = "Needs an ID parameter in query")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Successfully updated"),
+            @ApiResponse(code = 400, message = "Invalid input")})
     public ResponseEntity<TestDTO> put(@RequestBody TestDTO testDTO) {
         if (testDTO == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         testDTO = testService.put(testDTO);
@@ -51,6 +66,10 @@ public class TestController {
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deletes a test", notes = "Deletes a test, if the test is referenced in another entity the deletion can't be completed")
+    @ApiResponses(value = {@ApiResponse(code = 204, message = "Successfully deleted"),
+            @ApiResponse(code = 404, message = "Test does not exists"),
+            @ApiResponse(code = 500, message = "Test is referenced in other entities (children)")})
     public ResponseEntity delete(@PathVariable Long id) {
         try {
             return testService.delete(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
--- src/main/java/com/appointments/spappoitmentsapi/dto/TestDTO.java
+++ src/main/java/com/appointments/spappoitmentsapi/dto/TestDTO.java
@@ -1,13 +1,16 @@
 package com.appointments.spappoitmentsapi.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
+@ApiModel
 public class TestDTO {
 
-    private Long id;
-    private String name;
-    private String description;
+    @ApiModelProperty(readOnly = true) private Long id;
+    @ApiModelProperty private String name;
+    @ApiModelProperty private String description;
 }
